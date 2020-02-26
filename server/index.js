2964a5ce68752a5597332097311a3be6289cfd20
willyserge <willyserge01@gmail.com>
2020-02-26 18:05:52 +0200

    configure babel and eslint

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+//import bodyParser from 'body-parser';
+
+const app = express();
+
+//app.use(bodyParser.json());
+
+app.get('/',(req,res) => {
+    res.send("Hello Babel")
+})
+
+app.listen(3000,() => {
+    console.log(`app is listening to port 4000`);
+})
+
