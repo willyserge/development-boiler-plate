4e66fc9607dc1a0ba7753fd81cbf11bcc14dc58d
willyserge <willyserge01@gmail.com>
2020-02-26 18:28:15 +0200

    add tests

--- server/index.js
+++ server/index.js
@@ -1,15 +1,17 @@
 import express from 'express';
-//import bodyParser from 'body-parser';
+// import bodyParser from 'body-parser';
 
 const app = express();
 
-//app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
-app.get('/',(req,res) => {
-    res.send("Hello Babel")
-})
+app.get('/', (req, res) => {
+  res.status(200).send('welcome to the api');
+});
 
-app.listen(3000,() => {
-    console.log(`app is listening to port 4000`);
-})
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('app is listening to port 4000');
+});
 
+export default app;
---
+++ server/tests/app.test.js
@@ -0,0 +1,19 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../index.js';
+
+const { expect } = chai;
+chai.use(chaiHttp);
+describe("Server!", () => {
+  it("welcomes user to the api", done => {
+    chai
+      .request(app)
+      .get("/")
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+
+
+});
